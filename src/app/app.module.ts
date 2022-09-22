6c569a33397cd05f132e016306d8b0e00f8293c6
PMG <pattoyo01@gmail.com>
2022-09-22 03:55:26 -0300

    retomando proecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/miscarpetas/header/header.component';
+
+import { LogoAPComponent } from './components/miscarpetas/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/miscarpetas/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
